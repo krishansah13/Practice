94e9162fed7f830b075c5df7b54ebe3ddc9406df
Krishan <krishan.sah@geekyants.com>
2026-06-26 15:54:07 +0530

    Did Come callbacks too

--- learnGeeky/Arrays/Methods.js
+++ learnGeeky/Arrays/Methods.js
@@ -21,3 +21,21 @@ console.log(arr.filter((num)=>num%3 == 0))
 
 for(let ele of arr) console.log(ele);
 console.log(arr.reduce((sum, n)=>sum+n, 0))
+
+console.log(arr.some((num)=>num%2!=0))
+
+arr.forEach(num=>console.log(num));
+
+function fetchData(callback) {
+  console.log("Fetching data...");
+  setTimeout(() => {
+    const data = { name: "Alice", age: 25 };
+    callback(data);
+  }, 2000);  // Simulate a delay of 2 seconds
+}
+
+function processData(data) {
+  console.log("Data received:", data);
+}
+
+fetchData(processData)
